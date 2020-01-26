f9960c60a809223aace55a40184a5e22223ff7eb
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-01-26 13:55:33 +0100

    It works but the code is bad

--- js/main.js
+++ js/main.js
@@ -1,1 +1,104 @@
-console.log("I'm the main");
+
+// Canvas
+
+let canvas = document.getElementById("game");
+let ctx = canvas.getContext("2d");
+
+// Img
+
+let background;
+
+// The Spaceman - Player
+
+let player = {
+  x: 220,
+  y: canvas.height - 80,
+  width: 50,
+  height: 50,
+}
+
+let keyboard = {};
+
+// Functions
+
+
+function loadMedia(){
+  background = new Image();
+  background.src = "img/background.jpg";
+  background.onload = function(){
+    let interval = window.setInterval(update, 1000/55);
+  }
+} 
+
+function drawBackground(){
+  ctx.drawImage(background,0,0); 
+}
+
+// Funciones del player - En teoría pertenecería a Game 
+
+function createPlayer(){
+  ctx.save();
+  ctx.fillStyle = "red";
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+  ctx.restore();
+}
+
+// Función que creo que va en Game también según el juego del snake
+
+function assignControlsToKeys(){
+  // Cuando el usuario le da a una tecla se convierte en true
+    addEvent(document,"keydown", function(e){
+      keyboard[e.keyCode] = true;
+    });
+    addEvent(document,"keyup", function(e){
+      keyboard[e.keyCode] = false;
+    });
+
+  function addEvent(element, name, callback){
+    if(element.addEventListener){
+      // Navegadores buenos
+       element.addEventListener(name, callback, false);   
+    }
+    else if(element.attachEvent){
+      // Para internet explorer
+      elemento.attachEvent(name, callback);
+    }
+  }  
+}
+
+function movePlayer(){
+  if(keyboard[37]){
+    // Movimiento a la izquierda
+    player.x -= 10;
+      if (player.x < 0) player.x = 0;
+  }
+  if(keyboard[39]){
+    // Movimiento a la izquierda
+    let limit = canvas.width - player.width;
+    player.x += 10;
+      if (player.x > limit) player.x = limit;
+  }
+
+}
+
+function update(){
+  movePlayer();
+  drawBackground();
+  createPlayer();
+}
+
+// Call functions
+assignControlsToKeys(); 
+loadMedia();
+
+
+
+
+
+
+
+
+
+
+
+  
--- js/theSpaceman.js
+++ js/theSpaceman.js
@@ -1,1 +1,119 @@
 console.log("I'm the Spaceman");
+
+class Snake {
+    constructor(maxRows, maxColumns) {
+      this.initialBody = [
+        { row: 1, column: 5 },
+        { row: 1, column: 4 },
+        { row: 1, column: 3 },
+        { row: 1, column: 2 },
+        { row: 1, column: 1 },
+      ];
+      this.body = [...this.initialBody];
+      this.maxRows = maxRows;
+      this.maxColumns = maxColumns;
+      this.direction = 'up';
+      this.intervalId = undefined;
+    }
+  
+    _moveForward() {
+      var head = this.body[0];
+      switch (this.direction) {
+        case "up":
+          this.body.unshift({
+            row: (head.row - 1 + this.maxRows) % this.maxRows,
+            column: head.column
+          });
+          break;
+        case "down":
+          this.body.unshift({
+            row: (head.row + 1) % this.maxRows,
+            column: head.column
+          });
+          break;
+        case "left":
+          this.body.unshift({
+            row: head.row,
+            column: (head.column - 1 + this.maxColumns) % this.maxColumns
+          });
+          break;
+        case "right":
+          this.body.unshift({
+            row: head.row,
+            column: (head.column + 1) % this.maxColumns
+          });
+          break;
+      }
+      this.previousTail = this.body.pop();
+    }
+  
+    goUp() {
+      if (this.direction === "left" || this.direction === "right") {
+        this.direction = "up";
+      }
+    }
+  
+    goDown() {
+      if (this.direction === "left" || this.direction === "right") {
+        this.direction = "down";
+      }
+    }
+  
+    goLeft() {
+      if (this.direction === "up" || this.direction === "down") {
+        this.direction = "left";
+      }
+    }
+  
+    goRight() {
+      if (this.direction === "up" || this.direction === "down") {
+        this.direction = "right";
+      }
+    }
+  
+    move() {
+      this.intervalId = setInterval(this._moveForward.bind(this), 100);
+    }
+  
+    grow() {
+      if (this.previousTail) {
+        this.body.push(this.previousTail);
+        this.previousTail = undefined;
+      }
+    }
+  
+    hasEatenItSelf() {
+      // the only way to check if the head's position is inside of my body
+      return this.body.some((element, index, array) => {
+        return (
+          element.row === array[0].row &&
+          element.column === array[0].column &&
+          index != 0
+        );
+      });
+    }
+  
+    collidesWith(position) {
+      return this.body.some(bodyPiece => {
+        return (
+          bodyPiece.row === position.row && bodyPiece.column === position.column
+        );
+      });
+    }
+  
+    reset() {
+      this.body = [...this.initialBody];
+      this.direction = 'right';
+    }
+  
+    stop() {
+      if (this.intervalId) {
+        clearInterval(this.intervalId);
+        this.intervalId = undefined;
+      }
+    }
+  }
+
+
+
+
